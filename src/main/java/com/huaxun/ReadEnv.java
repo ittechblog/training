7595747e3a1ff6994babbe7767e3889f236f087b
yangyang2183 <yangyang2183@sina.com>
2016-03-01 11:08:55 +0800

    读取properties

--- src/main/java/com/huaxun/ReadEnv.java
+++ src/main/java/com/huaxun/ReadEnv.java
@@ -1,16 +1,23 @@
 package com.huaxun;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Created by Administrator on 2016/2/29.
  */
 public class ReadEnv {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         ReadEnv readEnv = new ReadEnv();
         readEnv.read();
     }
 
-    public void read(){
-        String path = getClass().getClassLoader().getResource("dev.properties").getPath();
-        System.out.println(path);
+    public void read() throws IOException {
+        String path = getClass().getClassLoader().getResource("test.properties").getPath();
+        Properties prop =  new Properties();
+        FileInputStream in = new FileInputStream(path);
+        prop.load(in);
+        System.out.println(prop.getProperty("name"));
     }
 }
